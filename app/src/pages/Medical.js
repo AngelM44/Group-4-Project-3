46093a0cd462bc02c4018a519dd45fd1218765f6
Davth5 <davth5@gmail.com>
2023-08-31 15:09:26 -0600

    Last Link added--medical ID

--- app/src/pages/Medical.js
+++ app/src/pages/Medical.js
@@ -2,50 +2,53 @@ import { Button, Typography, Container, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Logo from "../components/Logo";
 //////////////////////////////////////
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { styled } from '@mui/material/styles';
-import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled } from "@mui/material/styles";
+import { Link, useParams } from "react-router-dom";
 import MedicalTable from "./MedicalTable";
 import { useEffect, useState } from "react";
 
 const colorPalette = {
-    primaryDark: "olive",
-    primary: "light blue",
-    secondary: "orange",
-    tertiary: "white",
-    neutral: "white",
-  };
-  const StyledLink = styled(Link)({
-    textDecoration: "none",
-    color: colorPalette.tertiary,
-    marginLeft: "1rem",
-    "&:hover": {
-      color: colorPalette.secondary,
-    },
-  });
-  const StyledAppBar = styled(AppBar)({
-    backgroundColor: colorPalette.primaryDark,
-  });
+  primaryDark: "olive",
+  primary: "light blue",
+  secondary: "orange",
+  tertiary: "white",
+  neutral: "white",
+};
+const StyledLink = styled(Link)({
+  textDecoration: "none",
+  color: colorPalette.tertiary,
+  marginLeft: "1rem",
+  "&:hover": {
+    color: colorPalette.secondary,
+  },
+});
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: colorPalette.primaryDark,
+});
 
 const Medical = ({ setAuth }) => {
   const [medicalData, setMedicalData] = useState([]);
-  //const { id } = useParams();
+  const { id } = useParams();
   useEffect(() => {
-    fetch(`http://localhost:8080/medical/`)
+    const url = id
+      ? `http://localhost:8080/medical/${id}`
+      : "http://localhost:8080/medical/";
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setMedicalData(data))
-      .catch(err => console.log(medicalData))
-  }, []);
+      .catch((err) => console.log(medicalData));
+  }, [id]);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <StyledAppBar position="static">
         <Toolbar>
           <img
-            src="the-mandalorian-helmet-png-image-free-transparent-download-high-quality-images-768x1126.png"
+            src="https://pnghq.com/wp-content/uploads/the-mandalorian-helmet-png-image-free-transparent-download-high-quality-images-768x1126.png"
             alt="logo"
             style={{ height: 45, marginRight: 10 }}
           />
@@ -77,11 +80,10 @@ const Medical = ({ setAuth }) => {
         </Toolbar>
       </StyledAppBar>
       <center>
-        <MedicalTable data={medicalData}/>
+        <MedicalTable data={medicalData} />
       </center>
     </Box>
   );
 };
-
 
 export default Medical;
--- app/src/pages/MedicalTable.js
+++ app/src/pages/MedicalTable.js
@@ -1,37 +1,39 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
 
-export default function MedicalTable({data}) {
-  console.log(data.data);
+export default function MedicalTable({ data }) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>ID</TableCell>
-            <TableCell align="right">status</TableCell>
-            <TableCell align="right">checkup due by</TableCell>
-            <TableCell align="right">immunization due</TableCell>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">Checkup Due By</TableCell>
+            <TableCell align="right">Immunization Due</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((row) => (
             <TableRow
               key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.id}
+                <Link to={`/medical/${row.id}`}>{row.id}</Link>
               </TableCell>
               <TableCell align="right">{row.status}</TableCell>
-              <TableCell align="right">{row['checkup due by']}</TableCell>
-              <TableCell align="right">{row['immunization due'] ? 'yes' : 'no'}</TableCell>
+              <TableCell align="right">{row["checkup due by"]}</TableCell>
+              <TableCell align="right">
+                {row["immunization due"] ? "yes" : "no"}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
